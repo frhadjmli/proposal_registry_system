5e4e1de2cb1b3d2961123ee90c244c62a205153c
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-01-29 19:30:47 +0330

    add urls.py in account and include it to main url(config)

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
     path('', include('document.urls')),
+    path('account/', include('account.urls')),
 ]
