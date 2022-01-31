6d32568d923cb5ec2ad1156692223f670d38d7fb
farhad <farhad080.jamali@gmail.com>
2022-01-31 17:35:44 +0330

    fix minor logical errors in models

--- account/models.py
+++ account/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from document.models import Proposal
 
 
 class User(AbstractUser):
@@ -11,9 +10,11 @@ class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     std_number = models.CharField(max_length=10)
     field = models.CharField(max_length=50)
-    proposal = models.ManyToManyField(Proposal)
     taken_unit = models.PositiveIntegerField()
     passed_unit = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.user.get_full_name()} std_number : {self.std_number}"
 
 
 class Supervisor(models.Model):
@@ -26,12 +27,20 @@ class Supervisor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     academic_rank = models.CharField(max_length=3, choices=WorkingArea.choices)
     working_area = models.CharField(max_length=100, null=True)
-    proposal = models.ForeignKey(Proposal, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.get_full_name()
 
 
 class HOD(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.get_full_name()
+
 
 class DprtAdmin(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.get_full_name()
