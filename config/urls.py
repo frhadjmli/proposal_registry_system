4eabf2446654ef61576f8dfc9d45e9ca32372e75
farhad <farhad080.jamali@gmail.com>
2022-01-31 17:41:52 +0330

    add this path 'document/' to main urls.py

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
-    path('', include('document.urls')),
+    path('document/', include('document.urls')),
 ]
