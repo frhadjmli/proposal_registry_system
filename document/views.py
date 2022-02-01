ab7e2cb9d7183e7fd48ace993d71086a5f23103f
farhad <farhad080.jamali@gmail.com>
2022-02-01 17:38:39 +0330

    create document view and working on it

--- document/views.py
+++ document/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from document.models import Proposal
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+def submit_proposal(request):
+    if request.method == 'POST':
+        try:
+            proposal = Proposal()
+            proposal.title = request.POST.get('...')
+            proposal.supervisor = request.POST.get('...')
+            proposal.student = request.POST.get('...')
+            proposal.semester = request.POST.get('...')
+            proposal.academic_year = request.POST.get('...')
+            proposal.summary = request.POST.get('...')
+            proposal.message = None
+            proposal.save()
+            response = "Proposal added Successfuly to database."
+            return HttpResponse(response)
+        except:
+            response = "Proposal failed to add in database!"
+            return HttpResponse(response)
+        
