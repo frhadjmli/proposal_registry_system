5d0d5f9940fb374a0f2b27e2500287776296a5f2
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-02-02 21:19:12 +0330

    add name option in urlpatterns in account\urls.py

--- account/urls.py
+++ account/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login),
-    path('logout/', views.logoutUser),
-    path('student_profile/', views.student_profile),
-    path('group_up/', views.group_up),
-    path('supervisors_list/', views.supervisors_list),
-    path('supervisor_profile/', views.supervisor_profile),
-    path('hod_profile/', views.hod_profile),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('student_profile/', views.student_profile, name='student_profile'),
+    path('group_up/', views.group_up, name='group_up'),
+    path('supervisors_list/', views.supervisors_list, name='supervisors_list'),
+    path('supervisor_profile/', views.supervisor_profile, name='supervisor_profile'),
+    path('hod_profile/', views.hod_profile, name='hod_profile'),
 ]
+# use option name in return redirect() in views.py
+# use option name in {% url 'logout'%} in student_profile.html
