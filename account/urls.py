bf312147d2800e4e08c2e42c819f530789bd0123
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-02-04 19:53:53 +0330

    add req_to_lecturer to path ( account\urls.py )

--- account/urls.py
+++ account/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('supervisors_list/', views.supervisors_list, name='supervisors_list'),
     path('supervisor_profile/', views.supervisor_profile, name='supervisor_profile'),
     path('hod_profile/', views.hod_profile, name='hod_profile'),
+    path('req_to_lecturer/', views.req_to_lecturer, name='req_to_lecturer'),
 ]
 # use option name in return redirect() in views.py
 # use option name in {% url 'logout'%} in student_profile.html
