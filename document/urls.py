1a1461641fe100d4c9315703316b3d875ae60274
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-02-04 05:38:38 +0330

    add name option

--- document/urls.py
+++ document/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from document import views
 
 urlpatterns = [
-    path('submit_proposal/', views.submit_proposal),
-    path('proposal_maker/', views.proposal_maker),
-    path('view_proposal/', views.view_proposal),
+    path('submit_proposal/', views.submit_proposal, name='submit_proposal'),
+    path('proposal_maker/', views.proposal_maker, name='proposal_maker'),
+    path('view_proposal/', views.view_proposal, name='view_proposal'),
 ]
