b1f27d11020886eae9c201a03b8fc3b5781671ca
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-02-04 21:41:34 +0330

    set  null = True  for proposal in Message ( document\models.py )
    because in DB previous value of proposal is not define

--- document/models.py
+++ document/models.py
@@ -31,5 +31,5 @@ class Message(models.Model):
     title = models.CharField(max_length=50, null=True)
     sender = models.ForeignKey(Supervisor, on_delete=models.DO_NOTHING)
     text = models.TextField(null=True, help_text="پیام خود را وارد کنید.")
-    proposal = models.ForeignKey(Proposal, on_delete=models.CASCADE)
+    proposal = models.ForeignKey(Proposal, on_delete=models.CASCADE, null=True)
 
