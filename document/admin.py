c3a25c8b1e3709b65f51beff38009ed84cfd42f5
farhad <farhad080.jamali@gmail.com>
2022-02-06 20:22:59 +0330

    add 'get_supervisors' and 'get_students' to list_display in ProposalAdmin

--- document/admin.py
+++ document/admin.py
@@ -7,7 +7,7 @@ class MessageInline(admin.StackedInline):
 
 @admin.register(Proposal)
 class ProposalAdmin(admin.ModelAdmin):
-    list_display = ['title', 'semester',
+    list_display = ['title', 'get_supervisors', 'get_students', 'semester',
                     'academic_year', 'summary', 'status']
     sortable_by = ['semester', 'academic_year']
     list_filter = ['supervisor', 'status', 'semester', 'academic_year']
