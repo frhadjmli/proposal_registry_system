aa2d353207731cfcd7e51f64261dafd2d4446e79
farhad <farhad080.jamali@gmail.com>
2022-02-06 18:30:49 +0330

    create 'admin.py' for 'document' app

--- document/admin.py
+++ document/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Proposal, Message
 
 # Register your models here.
+class MessageInline(admin.StackedInline):
+    model = Message
+
+@admin.register(Proposal)
+class ProposalAdmin(admin.ModelAdmin):
+    list_display = ['title', 'supervisor', 'student', 'semester',
+                    'academic_year', 'summary', 'status']
+    sortable_by = ['semester', 'academic_year']
+    list_filter = ['supervisor', 'status', 'semester', 'academic_year']
+    search_fields = ['title', 'supervisor', 'student']
+    inlines = [MessageInline]
+
+admin.site.register(Message)
