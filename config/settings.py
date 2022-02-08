fbcf5839349eae4e00128fdd2c91d16ce91f0bbc
farhad <farhad080.jamali@gmail.com>
2022-02-08 11:01:58 +0330

    DB settings needed to be committed

--- config/settings.py
+++ config/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'prs_db',
         'USER': 'postgres', 
-        'PASSWORD': '1234',
+        'PASSWORD': 'oathkeeper',
         'HOST': '127.0.0.1', 
         'PORT': '5432',
     }
