1aa3b15afa3073e019b89a6a8b1ae40bd7603523
aminShahbaghi <a.shahbaghi@yahoo.com>
2022-06-11 21:07:18 +0430

    add url to accept_proposal

--- document/urls.py
+++ document/urls.py
@@ -4,4 +4,5 @@ from document import views
 urlpatterns = [
     path('submit_proposal/', views.submit_proposal, name='submit_proposal'),
     path('view_proposal/<int:pk>/', views.view_proposal, name='view_proposal'),
+    path('accept_proposal/<int:pk>/', views.accept_proposal, name='accept_proposal'),
 ]
